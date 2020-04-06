cdc30908f91ab253696bb9560e9d7b056a228c60
TXW <tlangelani.maswanganye@rmb.co.za>
2020-04-06 15:24:13 +0200

    Working around routing and adding router guard on about us and dashboard. Added a page not found component.

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -4,6 +4,12 @@ import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 import {AboutUsComponent} from '../../pages/about-us/about-us.component';
 
 export const AdminLayoutRoutes: Routes = [
-  { path: 'dashboard', component: DashboardComponent},
-  { path: 'aboutUs', component: AboutUsComponent }
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'aboutUs',
+    component: AboutUsComponent
+  },
 ];
